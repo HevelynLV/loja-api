6aadb72d4a271cf2d6ff75255c56eed2cc0cdeda
Carolliny-cmd <carollinygrundler@gmail.com>
2026-06-18 13:57:03 -0300

    fix: adicionando modo headless ao selenium para rodar no CI

--- tests/selenium.test.js
+++ tests/selenium.test.js
@@ -1,11 +1,19 @@
 const { Builder, By } = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome'); // <-- 1. Adicionado para configurar o Chrome
 
 describe('Teste de Interface', () => {
   let driver;
 
   beforeAll(async () => {
+    // 2. Configura o Chrome para rodar em modo oculto (Headless) no GitHub Actions
+    let options = new chrome.Options();
+    options.addArguments('--headless=new'); 
+    options.addArguments('--no-sandbox');
+    options.addArguments('--disable-dev-shm-usage');
+
     driver = await new Builder()
       .forBrowser('chrome')
+      .setChromeOptions(options) // <-- 3. Passa as configurações para o navegador
       .build();
   }, 30000);
 
